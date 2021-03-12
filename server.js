4214fef12d1ff509ebedc53244db8460939981cc
Mateo <mtaycruz@gmail.com>
2021-03-12 14:22:40 -0500

    format plural and format dated now works

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -22,10 +19,15 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
+
 
 
 app.engine('handlebars', hbs.engine);
